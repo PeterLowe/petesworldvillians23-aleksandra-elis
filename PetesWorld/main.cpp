841b73054d17ecad4a5f0df9954f5688df1709c7
(Student  C00294397) Aleksandra Skarzycka <C00294397@setu.ie>
2023-11-10 09:35:07 +0000

    aleksandra

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -1,8 +1,8 @@
 // program to find countries in Pete's World
-// date November 2022
+// date 10 November 2023
 // author Pete Lowe
 // add your name here
-//  or here
+// Aleksandra Skarzycka
 // estimated time 120 minutes
 // actual time ??
 // version 1.0
