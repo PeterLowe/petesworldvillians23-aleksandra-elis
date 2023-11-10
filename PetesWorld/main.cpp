e7876e9800287b4ba38aa35afe785f75cc0ef95f
(Student  C00294397) Aleksandra Skarzycka <C00294397@setu.ie>
2023-11-10 10:15:07 +0000

    bAleksandra

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -55,6 +55,30 @@ int main()
 			std::cout << "Australia, Antarctica, Aruba, Anguilla Leeward Islands, Antigua and Barbuda, American Samoa" << std::endl;
 		}
 	}
+}
+if (letter == 'b' || letter == 'B')
+{
+	if (region == 1)
+	{
+		std::cout << "Belize, Bermuda, Bolivia, Brazil" << std::endl;
+	}
+	if (region == 2)
+	{
+		std::cout << "Belarus, Belgium, Bosnia and Herzegovina, Bulgaria" << std::endl;
+	}
+	if (region == 3)
+	{
+		std::cout << "Benin, Botswana, Burkina Faso, Burundi" << std::endl;
+	}
+	if (region == 4)
+	{
+		std::cout << "Bangladesh, Bhutan, Brunei Darussalam" << std::endl;
+	}
+	if (region == 5)
+	{
+		std::cout << "Bahamas, Bahrain, Barbados" << std::endl;
+	}
+}
 	if (letter == 'x' || letter == 'X')
 	{
 		if (region == 1)
